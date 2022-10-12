b229348b7f32a397d9a5c4ea173bf41f76d08a6b
Jihoo <53351097+wildcatco@users.noreply.github.com>
2022-10-12 17:37:46 +0900

    login form validation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,60 @@
+import { useForm } from 'react-hook-form';
+
+interface ILoginForm {
+  email: string;
+  password: string;
+}
+
 const Login = () => {
+  const {
+    register,
+    getValues,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<ILoginForm>();
+
+  const onSubmit = () => {
+    console.log(getValues());
+  };
+
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
       <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
         <h3 className="text-3xl text-gray-800">Log In</h3>
-        <form className="flex flex-col mt-5 px-5">
-          <input className="input mb-3" placeholder="Email" />
-          <input className="input " placeholder="Password" />
+        <form
+          className="grid gap-3 mt-5 px-5"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <input
+            {...register('email', { required: 'Email is required' })}
+            type="email"
+            placeholder="Email"
+            required
+            className="input"
+          />
+          {errors.email?.message && (
+            <span className="font-medium text-red-500">
+              {errors.email.message}
+            </span>
+          )}
+          <input
+            {...register('password', {
+              required: 'Password is required',
+              minLength: {
+                value: 8,
+                message: 'Password must be more than 8 characters',
+              },
+            })}
+            type="password"
+            placeholder="Password"
+            required
+            className="input "
+          />
+          {errors.password?.message && (
+            <span className="font-medium text-red-500">
+              {errors.password.message}
+            </span>
+          )}
           <button className="btn mt-3">Log In</button>
         </form>
       </div>

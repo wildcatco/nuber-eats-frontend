a2b86b26f3e07d93f7ea08703f348e1c110bb745
Jihoo <53351097+wildcatco@users.noreply.github.com>
2022-10-20 02:48:50 +0900

    restaurants query

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,6 +1,48 @@
+import { useQuery } from '@apollo/client';
 import React from 'react';
+import { graphql } from './../../gql/gql';
+
+const RESTAURANTS_QUERY = graphql(`
+  query restaurantsPageQuery($input: RestaurantsInput!) {
+    categories {
+      ok
+      error
+      categories {
+        id
+        name
+        coverImg
+        slug
+        restaurantCount
+      }
+    }
+    restaurants(input: $input) {
+      ok
+      error
+      totalPages
+      totalResults
+      results {
+        id
+        name
+        coverImg
+        category {
+          name
+        }
+        address
+        isPromoted
+      }
+    }
+  }
+`);
 
 const Restaurants: React.FC = () => {
+  const { data, loading, error } = useQuery(RESTAURANTS_QUERY, {
+    variables: {
+      input: {
+        page: 1,
+      },
+    },
+  });
+
   return <h1>Restaurants</h1>;
 };
 
